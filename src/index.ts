dec7e748c6d23d9aad11db3f8e1bac5321f7a8d3
Andrei Pampu <pampu.s.andrei@gmail.com>
2018-12-03 21:20:57 +0200

    Add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import Koa from 'koa';
+import mongoose from 'mongoose';
+
+const port = 8081;
+const mongoUrl = 'mongo:27017/dashboard';
 
 class App {
     app: Koa;
-    
+
     constructor() {
         this.app = new Koa();
 
         this.app.use(async ctx => {
-            ctx.body = 'Hello World';
+            ctx.body = 'Hello Docker!';
         });
 
-        this.app.listen(8081);
+        this.app.listen(port);
+        console.log(`Server is running on port ${port}`);
+
+        mongoose.connect(`mongodb://${mongoUrl}`, {
+            useNewUrlParser: true
+        }).then(() => {
+            console.log('Succesfuly connected to the database')
+        }).catch(e => {
+            console.log('Couldn\'t connect to the database: ' + e);
+        })
     }
 }
 
