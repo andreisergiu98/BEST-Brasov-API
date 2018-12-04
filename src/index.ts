27d9b3e4ba5abebb1f5a02bf6b2164e2c47440cb
Andrei Pampu <pampu.s.andrei@gmail.com>
2018-12-04 23:56:36 +0200

    Add middlewares and routes to app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import Koa from 'koa';
-import mongoose from 'mongoose';
+import logger from 'koa-logger';
+import bodyparser from 'koa-bodyparser';
+
+import Db from './core/db';
+import router from './routes';
 
 const port = 8081;
 const mongoUrl = 'mongo:27017/dashboard';
@@ -8,28 +12,15 @@ class App {
     app: Koa;
 
     constructor() {
-        mongoose.connect(`mongodb://${mongoUrl}`, {
-            useNewUrlParser: true
-        }).then(() => {
-            console.log('Succesfuly connected to the database')
-        }).catch(e => {
-            console.log('Couldn\'t connect to the database: ' + e);
-        })
+        Db.connect(`mongodb://${mongoUrl}`);
 
         this.app = new Koa();
 
-        this.app.use(async (ctx, next) => {
-            await next();
-            const rt = ctx.response.get('X-Response-Time');
-            console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-        });
+        this.app.use(logger());
 
-        this.app.use(async (ctx, next) => {
-            const start = Date.now();
-            await next();
-            const ms = Date.now() - start;
-            ctx.set('X-Response-Time', `${ms}ms`);
-          });
+        this.app.use(bodyparser());
+
+        this.app.use(router);
 
         this.app.listen(port);
         console.log(`Server is running on port ${port}`);
