1ddc2cfec42a7bfaa353bba600e5a7bce6b28688
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-02-10 17:15:47 +0200

    Add GET entity route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+
 import * as user from './user';
 import * as entity from './entity';
 
@@ -9,6 +10,7 @@ router.get('/user/:id', user.getById);
 router.get('/users', user.getAllUsers);
 
 // Entity requests
+router.get('/entity/:id', entity.getById);
 router.get('/entities', entity.getAllEntities);
 
 export default router.routes();
