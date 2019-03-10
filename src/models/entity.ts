5d0e5dd92888049724e89acc973c109214a08b13
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-03-10 20:07:28 +0200

    Link categories to entities and enable autopopulation

--- src/models/entity.ts
+++ src/models/entity.ts
@@ -1,7 +1,7 @@
 import {Document, Schema, model} from "mongoose";
 
 export interface IEntity extends Document {
-    name?: string;
+    name: string;
     city?: string;
     address?: string;
     categories?: [string];
@@ -15,12 +15,14 @@ export const EntitySchema = new Schema({
     name: {type: String, required: true},
     city: String,
     address: String,
-    categories: [String],
+    categories: [{type: Schema.Types.ObjectId, ref: 'Category', autopopulate: true}],
     website: String,
     phoneNumbers: [{phone: String, info: String}],
     emailAddresses: [{email: String, info: String}],
     numberOfCalls: Number
 });
+
+EntitySchema.plugin(require('mongoose-autopopulate'));
 
 const Entity = model<IEntity>('Entity', EntitySchema);
 export default Entity;
