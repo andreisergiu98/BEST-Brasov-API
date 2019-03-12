372701ba17ee7decd173a85d7c158d428bc377e0
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-03-13 00:07:07 +0200

    Throw 404 if the id is invalid

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,7 +5,7 @@ import User from '../models/user';
 
 export const getById = async (ctx: Koa.Context, next: Function) => {
     if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
-        ctx.throw(400, 'Invalid data!');
+        ctx.throw(404);
     }
 
     let response = await User.findById(ctx.params.id);
@@ -13,7 +13,7 @@ export const getById = async (ctx: Koa.Context, next: Function) => {
         ctx.status = 200;
         ctx.body = response;
     } else {
-        ctx.throw(404, 'User not found');
+        ctx.throw(404);
     }
 };
 
