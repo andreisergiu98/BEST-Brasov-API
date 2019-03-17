c9cd833529364f8b965b34ffe51abd1cc5419f7b
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-03-17 14:53:54 +0200

    Create ids for db data. Add mock data for meetings

--- src/core/dev.ts
+++ src/core/dev.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import User, {IUser} from '../models/user';
 import Entity, {IEntity} from '../models/entity';
 import Category, {ICategory} from "../models/category";
+import Meeting, {IMeeting} from "../models/meeting";
 
 export const init = (): void => {
     loadDbSeed().then().catch(e => {
@@ -15,6 +16,7 @@ const loadDbSeed = async () => {
     let users: IUser[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/users.json'), 'utf8'));
     let entities: IEntity[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/entities.json'), 'utf8'));
     let categories: ICategory[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/categories.json'), 'utf8'));
+    let meetings: IMeeting[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/meetings.json'), 'utf-8'));
 
     await User.deleteMany({});
     await User.insertMany(users);
@@ -22,20 +24,9 @@ const loadDbSeed = async () => {
     await Category.deleteMany({});
     await Category.insertMany(categories);
 
-    for (let i = 0; i < entities.length; i++) {
-        let categories: string[] = [];
-
-        for (let j = 0; j < (entities[i].categories || []).length; j++) {
-            // @ts-ignore
-            let res = await Category.findOne({name: entities[i].categories[j]});
-            if (res != null)
-                categories.push(res._id);
-        }
-
-        // @ts-ignore
-        entities[i].categories = categories;
-    }
-
     await Entity.deleteMany({});
     await Entity.insertMany(entities);
+
+    await Meeting.deleteMany({});
+    await Meeting.insertMany(meetings);
 };
