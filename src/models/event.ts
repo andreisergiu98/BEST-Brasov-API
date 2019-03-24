9e42ad1629bf8b123c2b87489d00da4296affff0
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-03-24 18:24:50 +0200

    Update event schema

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,27 +1,42 @@
 import {Document, Schema, model} from "mongoose";
 import {IUser} from "./user";
+import {ICallingSession} from "./calling-session";
 
 export interface IEvent extends Document {
     name: string;
     coordinator: IUser | string;
-    coreTeam: [{ user: IUser | string, role: string }];
-    usefulLinks: [{ description: string, link: string }];
-    callingSession: string;
-    starts: Date;
-    ends: Date;
+    coreTeam: [{
+        user: IUser | string,
+        role: string
+    }];
+    usefulLinks: [{
+        description: string,
+        link: string
+    }];
+    callingSession: ICallingSession | string;
+    date: {
+        starts: Date;
+        ends: Date;
+    };
 }
 
 export const EventSchema = new Schema({
     name: {type: String, required: true},
-    coordinator: {type: Schema.Types.ObjectId, ref: 'User', autopopulate: true, required: true},
-    coreTeam: [{user: {type: Schema.Types.ObjectId, ref: 'User', autopopulate: true}, role: String}],
-    usefulLinks: [{description: String, link: String}],
-    callingSession: {type: Schema.Types.ObjectId, ref: ''},
-    starts: Date,
-    ends: Date
+    coordinator: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    coreTeam: [{
+        user: {type: Schema.Types.ObjectId, ref: 'User'},
+        role: String
+    }],
+    usefulLinks: [{
+        description: String,
+        link: String
+    }],
+    callingSession: {type: Schema.Types.ObjectId, ref: 'CallingSession'},
+    date: {
+        starts: Date,
+        ends: Date
+    }
 }, {timestamps: true});
-
-EventSchema.plugin(require('mongoose-autopopulate'));
 
 const Event = model<IEvent>('Event', EventSchema);
 export default Event;
