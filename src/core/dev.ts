64fee5571c2635c72fedc34b7247d84e4e0931ab
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-06-26 22:24:19 +0300

    Fix imports for dev.ts

--- src/core/dev.ts
+++ src/core/dev.ts
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-import Comment from '../models/comment';
-import User, {IUser} from '../models/user';
-import Entity, {IEntity} from '../models/entity';
-import EntityCategory, {IEntityCategory} from '../models/entity-category';
-import Meeting, {IMeeting} from '../models/meeting';
-import CallingSession, {ICallingSession} from '../models/calling-session';
-import Event, {IEvent} from '../models/event';
+import {UserModel, User} from '../models/user';
+import {EntityModel, Entity} from '../models/entity';
+import {EntityCategoryModel, EntityCategory} from '../models/entity-category';
+import {MeetingModel, Meeting} from '../models/meeting';
+// import {CallingSession, CallingSessionType} from '../models/calling-session';
+// import {Event, EventType} from '../models/event';
 
 export const init = (): void => {
     loadDbSeed().then().catch(e => {
@@ -16,31 +15,28 @@ export const init = (): void => {
 };
 
 const loadDbSeed = async () => {
-    const users: IUser[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/users.json'), 'utf8'));
-    const entities: IEntity[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/entities.json'), 'utf8'));
-    const categories: IEntityCategory[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/categories.json'), 'utf8'));
-    const meetings: IMeeting[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/meetings.json'), 'utf8'));
-    const events: IEvent[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/events.json'), 'utf8'));
-    const callingSessions: ICallingSession[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/calling-sessions.json'), 'utf8'));
+    const users: User[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/users.json'), 'utf8'));
+    const entities: Entity[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/entities.json'), 'utf8'));
+    const categories: EntityCategory[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/categories.json'), 'utf8'));
+    const meetings: Meeting[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/meetings.json'), 'utf8'));
+    // const events: EventType[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/events.json'), 'utf8'));
+    // const callingSessions: CallingSessionType[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../development/db/calling-sessions.json'), 'utf8'));
 
-    await User.deleteMany({});
-    await User.insertMany(users);
+    await UserModel.deleteMany({});
+    await UserModel.insertMany(users);
 
-    await Comment.deleteMany({});
-    await Comment.insertMany([]);
+    await EntityCategoryModel.deleteMany({});
+    await EntityCategoryModel.insertMany(categories);
 
-    await EntityCategory.deleteMany({});
-    await EntityCategory.insertMany(categories);
+    await EntityModel.deleteMany({});
+    await EntityModel.insertMany(entities);
 
-    await Entity.deleteMany({});
-    await Entity.insertMany(entities);
+    await MeetingModel.deleteMany({});
+    await MeetingModel.insertMany(meetings);
 
-    await Meeting.deleteMany({});
-    await Meeting.insertMany(meetings);
+    // await Event.deleteMany({});
+    // await Event.insertMany(events);
 
-    await Event.deleteMany({});
-    await Event.insertMany(events);
-
-    await CallingSession.deleteMany({});
-    await CallingSession.insertMany(callingSessions);
+    // await CallingSession.deleteMany({});
+    // await CallingSession.insertMany(callingSessions);
 };
