5d07aee89a336b5f6aa7536e64a05bd15cc667cf
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-06-26 22:27:30 +0300

    Fix imports for routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,18 @@
 import Router from 'koa-router';
 
-import * as users from './users';
-import * as entities from './entities';
-import * as meetings from './meetings';
-import * as events from './events';
-import * as callingSessions from './callings-sessions';
+// import * as events from './events';
+// import * as callingSessions from './calling-sessions';
+import {UsersController} from '../controllers/users';
+import {EntitiesController} from '../controllers/entities';
+import {MeetingsController} from '../controllers/meetings';
 
 const router: Router = new Router();
 
-// User requests
+const users = new UsersController();
+const entities = new EntitiesController();
+const meetings = new MeetingsController();
+
+// userModel requests
 router.get('/users', users.getAll);
 router.get('/users/:id', users.getById);
 
@@ -17,18 +21,18 @@ router.get('/entities', entities.getAll);
 router.get('/entities/categories', entities.getCategories);
 router.post('/entities/comments', entities.addComment);
 router.get('/entities/:id', entities.getById);
-router.put('/entities', entities.update);
-router.post('/entities', entities.create);
+router.put('/entities', entities.updateOne);
+router.post('/entities', entities.createOne);
 
 // Meetings requests
 router.get('/meetings', meetings.getAll);
 
 // Events requests
-router.get('/events', events.getAll);
-router.get('/events/:id', events.getById);
+// router.get('/events', events.getAll);
+// router.get('/events/:id', events.getById);
 
 // Calling Sessions requests
-router.get('/calling-sessions', callingSessions.getAll);
-router.get('/calling-sessions/:id', callingSessions.getById);
+// router.get('/calling-sessions', callingSessions.getAll);
+// router.get('/calling-sessions/:id', callingSessions.getById);
 
 export const routes = router.routes();
