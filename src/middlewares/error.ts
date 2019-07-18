3fb4b41194f5bd5b04af5f21bee8d4a406287337
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-07-18 18:41:15 +0300

    Don't log intentional errors in production

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -5,8 +5,18 @@ export const catchError = async (ctx: Koa.Context, next: Function) => {
         await next();
     } catch (e) {
         ctx.status = e.status || 500;
-        ctx.body = ctx.status === 500 ? 'There was an error on the server, please report this to the coordinator!' : e.message;
-        ctx.app.emit('error', e, ctx);
+
+        if (ctx.status === 500) {
+            ctx.body = 'There was an error on the server, please report this to the coordinator!';
+            ctx.app.emit('error', e, ctx);
+            return;
+        }
+
+        if (process.env.NODE_ENV === 'development') {
+            console.log(e.message);
+        }
+
+        ctx.body = e.message;
     }
 };
 
