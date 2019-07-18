acab63cb0e08ebca275dacc763484c2a96e79dc1
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-07-18 20:05:54 +0300

    Connect to mongo async

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,48 +3,52 @@ import mongoose from 'mongoose';
 class Db {
     connection = mongoose.connection;
 
-    connect(url: string): void {
-        mongoose.set('useCreateIndex', true);
+    async connect(url: string) {
+        return new Promise((resolve, reject) => {
+            mongoose.set('useCreateIndex', true);
 
-        const db = this.connection;
+            const db = this.connection;
 
-        const config = {
-            useNewUrlParser: true,
-            autoReconnect: true,
-            useFindAndModify: false,
-        };
+            const config = {
+                useNewUrlParser: true,
+                autoReconnect: true,
+                useFindAndModify: false,
+            };
 
-        db.on('connecting', () => {
-            console.log('Connecting to Mongo...');
+            db.on('connecting', () => {
+                console.log('Connecting to Mongo...');
+            });
+
+            db.on('error', (error) => {
+                console.error(`Error in Mongo connection: ${error}`);
+                mongoose.disconnect();
+                reject();
+            });
+
+            db.on('connected', () => {
+                console.log('Mongo connected!');
+            });
+
+            db.once('open', () => {
+                console.log('Mongo connection opened!');
+                resolve();
+            });
+
+            db.on('reconnected', () => {
+                console.log('Mongo reconnected!');
+            });
+
+            db.on('disconnected', () => {
+                console.log('Mongo disconnected!');
+
+                // Wait 2 seconds and try to reconnect to the database
+                setTimeout(() => {
+                    mongoose.connect(url, config);
+                }, 2000);
+            });
+
+            mongoose.connect(url, config);
         });
-
-        db.on('error', (error) => {
-            console.error(`Error in Mongo connection: ${error}`);
-            mongoose.disconnect();
-        });
-
-        db.on('connected', () => {
-            console.log('Mongo connected!');
-        });
-
-        db.once('open', () => {
-            console.log('Mongo connection opened!');
-        });
-
-        db.on('reconnected', () => {
-            console.log('Mongo reconnected!');
-        });
-
-        db.on('disconnected', () => {
-            console.log('Mongo disconnected!');
-
-            // Wait 2 seconds and try to reconnect to the database
-            setTimeout(() => {
-                mongoose.connect(url, config);
-            }, 2000);
-        });
-
-        mongoose.connect(url, config);
     }
 }
 
