f3e288e4739b88cd655357c6bfde24197e66c070
Andrei Pampu <pampu.s.andrei@gmail.com>
2019-07-21 12:15:29 +0300

    Update promises for session storage, and create methods to update session data or to extend it.

--- src/lib/session-storage.ts
+++ src/lib/session-storage.ts
@@ -1,19 +1,35 @@
 import uuid from 'uuid/v4';
 import {RedisClient} from './redis';
 
+interface SessionData {
+    id: string;
+    name: string;
+    role: string;
+    expires?: number;
+}
+
 class SessionStorage extends RedisClient {
-    createSession(userId: string, data: {}, exp = 7 * 24 * 60 * 60) {
-        const key = userId + '-' + uuid();
-        return this.client.set(key, JSON.stringify(data), 'EX', exp);
+    defaultExp = 7 * 24 * 60 * 60;
+    private separator = '-';
+
+    createSession(userId: string, data: SessionData, exp = this.defaultExp): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const session = userId + this.separator + uuid();
+            data.expires = Date.now() + exp * 1000;
+
+            this.client.set(session, JSON.stringify(data), 'EX', exp)
+                .then(() => resolve(session))
+                .catch(e => reject(e));
+        });
     }
 
-    getSession(session: string) {
+    getSession(session: string): Promise<SessionData | undefined> {
         return new Promise((resolve, reject) => {
             this.client.get(session).then(res => {
                 if (res) {
                     resolve(JSON.parse(res));
                 } else {
-                    resolve({});
+                    resolve();
                 }
             }).catch(e => reject(e));
         });
@@ -26,13 +42,13 @@ class SessionStorage extends RedisClient {
     deleteAllSessions(userId: string) {
         return new Promise((resolve, reject) => {
             const stream = this.client.scanStream({
-                match: userId + '-*',
+                match: userId + this.separator + '*',
                 count: 100,
             });
 
             const keys: string[] = [];
 
-            stream.on('data', (resultKeys) => {
+            stream.on('data', resultKeys => {
                 for (let i = 0; i < resultKeys.length; i++) {
                     keys.push(resultKeys[i]);
                 }
@@ -43,6 +59,50 @@ class SessionStorage extends RedisClient {
                 // @ts-ignore
                 this.client.unlink(keys).then(() => resolve()).catch(e => reject(e));
             });
+        });
+    }
+
+    updateData(userId: string, data: SessionData) {
+        return new Promise((resolve, reject) => {
+            const stream = this.client.scanStream({
+                match: userId + this.separator + '*',
+                count: 100,
+            });
+
+            const keys: string[] = [];
+
+            stream.on('data', resultKeys => {
+                for (let i = 0; i < resultKeys.length; i++) {
+                    keys.push(resultKeys[i]);
+                }
+            });
+
+            const promises: Array<Promise<string>> = [];
+
+            stream.on('end', () => {
+                for (const key of keys) {
+                    let ttl = this.defaultExp;
+
+                    if (data.expires) {
+                        ttl = new Date(data.expires).getTime() - Date.now();
+                        ttl = Math.floor(ttl / 1000);
+                    }
+
+                    promises.push(this.client.set(key, JSON.stringify(data), 'EX', ttl));
+                }
+
+                Promise.all(promises).then(() => resolve()).catch(e => reject(e));
+            });
+        });
+    }
+
+    extendSession(session: string, data: SessionData, exp = this.defaultExp) {
+        return new Promise((resolve, reject) => {
+            data.expires = Date.now() + exp * 1000;
+
+            this.client.set(session, JSON.stringify(data), 'EX', exp)
+                .then(() => resolve(session))
+                .catch(e => reject(e));
         });
     }
 }
